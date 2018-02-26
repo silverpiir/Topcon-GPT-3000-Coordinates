9ccf1318c5e8f995e499a8a9070b978f758ee355
Silver Piir <35463981+silverpiir@users.noreply.github.com>
2018-02-26 14:50:48 +0200

    Update koordinaadid.py

--- koordinaadid.py
+++ koordinaadid.py
@@ -3,7 +3,7 @@ from tkinter import Menu, Tk, ttk
 from tkinter.filedialog import askopenfilename
 import ntpath
 
-def ava_fail():
+def open_file():
     nimi = askopenfilename(initialdir="/",
                            filetypes = (("Text File", "*.txt"),("All Files","*.*")),
                            title = "Vali lähtefail."
@@ -19,10 +19,10 @@ def ava_fail():
     #print(sisendi_read)
     
     sisend.close()
-    silt1 = tk.Label(aken, text = nimi)
+    silt1 = tk.Label(window, text = nimi)
     silt1.pack()
     
-def lisa_vaartused():
+def add_values():
     asi1 = (entry1.get())
     asi2 = (entry2.get())
         
@@ -38,21 +38,21 @@ def lisa_vaartused():
             valjund.write("\n")
     valjund.close()
                 
-    silt2 = tk.Label(aken, text = "Loodud programmiga samas kaustas " + "uus_" + valjundi_nimi)
+    silt2 = tk.Label(window, text = "Loodud programmiga samas kaustas " + "uus_" + valjundi_nimi)
     silt2.pack()
 
 # Järgneb kasutajaliidese akna kood
-aken = tk.Tk()
+window = tk.Tk()
 
-aken.title("Topcon GT-3000 koordinaadiprogramm")
-aken.geometry("500x300")
-#aken.wm_iconbitmap("prog.ico") ### Toimib, kui on samas kaustas prog.ico
+window.title("Topcon GT-3000 koordinaadiprogramm")
+window.geometry("500x300")
+#window.wm_iconbitmap("prog.ico") ### Toimib, kui on samas kaustas prog.ico
 
-label1 = tk.Label(aken, text = "X - koordinaat", font = ("Helvetica", 12))
-label2 = tk.Label(aken, text = "Y - koordinaat", font = ("Helvetica", 12))
-entry1 = tk.Entry(aken)
-entry2 = tk.Entry(aken)
-nupp = tk.Button(aken, text = "Lisa väärtused", command = lisa_vaartused, width = 20)
+label1 = tk.Label(window, text = "X - koordinaat", font = ("Helvetica", 12))
+label2 = tk.Label(window, text = "Y - koordinaat", font = ("Helvetica", 12))
+entry1 = tk.Entry(window)
+entry2 = tk.Entry(window)
+nupp = tk.Button(window, text = "Lisa väärtused", command = add_values, width = 20)
 
 label1.pack(pady = 10)
 entry1.pack(pady = 2)
@@ -60,12 +60,12 @@ label2.pack(pady = 10)
 entry2.pack(pady = 2)
 nupp.pack(pady = 10)
 
-menubar = Menu(aken)
-aken.config(menu=menubar)
+menubar = Menu(window)
+window.config(menu=menubar)
 fileMenu = Menu(menubar, tearoff = False)
 
-fileMenu.add_command(label = "Ava lähtefail", command= ava_fail)
-fileMenu.add_command(label = "Välju", command = aken.destroy)
-menubar.add_cascade(label = "Valikud", menu = fileMenu)
+fileMenu.add_command(label = "Open Source File", command= open_file)
+fileMenu.add_command(label = "Exit", command = window.destroy)
+menubar.add_cascade(label = "Options", menu = fileMenu)
 
-aken.mainloop()
+window.mainloop()
